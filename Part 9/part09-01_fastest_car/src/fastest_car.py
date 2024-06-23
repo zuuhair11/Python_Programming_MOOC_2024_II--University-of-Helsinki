0c147ccecacda5cda0e3784a09d0d19b63725ba4
Zouhair Sahtout <zouhairsahtout66@gmail.com>
2024-06-23 16:50:08 +0100

    The fastest car

---
+++ Part 9/part09-01_fastest_car/src/fastest_car.py
@@ -0,0 +1,28 @@
+# Write your solution after the class Car
+# Do not make changes to the class!
+class Car:
+    def __init__(self, make: str, top_speed: int):
+        self.make = make
+        self.top_speed = top_speed
+
+    def __str__(self):
+        return f'Car (make: {self.make}, top speed: {self.top_speed})'
+
+# WRITE YOUR SOLUTION HERE:
+def fastest_car(cars: list) -> str:
+    fastest = None
+    for car in cars:
+        if fastest == None or car.top_speed > fastest.top_speed:
+            fastest = car
+
+    return fastest.make
+
+
+if __name__ == '__main__':
+    car1 = Car('Saab', 195)
+    car2 = Car('Lada', 110)
+    car3 = Car('Ferrari', 280)
+    car4 = Car('Trabant', 85)
+
+    cars = [car1, car2, car3, car4]
+    print(fastest_car(cars))
