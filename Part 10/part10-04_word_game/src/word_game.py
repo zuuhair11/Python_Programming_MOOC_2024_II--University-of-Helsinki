19c62da8a45d5e9e38cb4571ffc4aa4a5594a621
Zouhair Sahtout <zouhairsahtout66@gmail.com>
2024-07-12 17:28:04 +0100

    Word game 2/3

--- Part 10/part10-04_word_game/src/word_game.py
+++ Part 10/part10-04_word_game/src/word_game.py
@@ -1,11 +1,12 @@
 # Write your solution here
+from typing import Optional
 import random
 
 class WordGame():
     def __init__(self, rounds: int):
-        self.wins1 = 0
-        self.wins2 = 0
-        self.rounds = rounds
+        self.wins1: int = 0
+        self.wins2: int = 0
+        self.rounds: int = rounds
 
     def round_winner(self, player1_word: str, player2_word: str):
         # determine a random winner
@@ -15,8 +16,8 @@ class WordGame():
         print('Word game:')
         for i in range(1, self.rounds+1):
             print(f'round {i}')
-            answer1 = input('player1: ')
-            answer2 = input('player2: ')
+            answer1: str = input('player1: ')
+            answer2: str = input('player2: ')
 
             if self.round_winner(answer1, answer2) == 1:
                 self.wins1 += 1
@@ -42,7 +43,29 @@ class LongestWord(WordGame):
         elif len(player2_word) > len(player1_word):
             return 2
 
-        return 0
+
+class MostVowels(WordGame):
+    def __init__(self, rounds: int):
+        super().__init__(rounds)
+
+    def __count_vowels(self, player_word: str) -> int:
+        vowels: str = 'aeiou'
+        count: int = 0
+
+        for letter in player_word:
+            if letter in vowels:
+                count += 1
+
+        return count
+
+    def round_winner(self, player1_word: str, player2_word: str) -> Optional[int]:
+        player1: int = self.__count_vowels(player1_word)
+        player2: int = self.__count_vowels(player2_word)
+
+        if player1 > player2:
+            return 1
+        elif player2 > player1:
+            return 2
 
 
 if __name__ == '__main__':
