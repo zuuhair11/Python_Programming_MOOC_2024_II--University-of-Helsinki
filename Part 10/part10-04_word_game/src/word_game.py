92d13c04f1eadf5d8a5206455ffba946d8c4fe81
Zouhair Sahtout <zouhairsahtout66@gmail.com>
2024-07-12 15:08:22 +0100

    Word game

---
+++ Part 10/part10-04_word_game/src/word_game.py
@@ -0,0 +1,50 @@
+# Write your solution here
+import random
+
+class WordGame():
+    def __init__(self, rounds: int):
+        self.wins1 = 0
+        self.wins2 = 0
+        self.rounds = rounds
+
+    def round_winner(self, player1_word: str, player2_word: str):
+        # determine a random winner
+        return random.randint(1, 2)
+
+    def play(self):
+        print('Word game:')
+        for i in range(1, self.rounds+1):
+            print(f'round {i}')
+            answer1 = input('player1: ')
+            answer2 = input('player2: ')
+
+            if self.round_winner(answer1, answer2) == 1:
+                self.wins1 += 1
+                print('player 1 won')
+            elif self.round_winner(answer1, answer2) == 2:
+                self.wins2 += 1
+                print('player 2 won')
+            else:
+                pass # it's a tie
+
+        print('game over, wins:')
+        print(f'player 1: {self.wins1}')
+        print(f'player 2: {self.wins2}')
+
+
+class LongestWord(WordGame):
+    def __init__(self, rounds: int):
+        super().__init__(rounds)
+
+    def round_winner(self, player1_word: str, player2_word: str):
+        if len(player1_word) > len(player2_word):
+            return 1
+        elif len(player2_word) > len(player1_word):
+            return 2
+
+        return 0
+
+
+if __name__ == '__main__':
+    p = LongestWord(3)
+    p.play()
