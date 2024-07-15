a54e3dcd00c8836d6f52e2fb83b2d7ea7c1fdfef
Zouhair Sahtout <zouhairsahtout66@gmail.com>
2024-07-15 19:21:16 +0100

    Money 2/5

--- Part 10/part10-07_money/src/money.py
+++ Part 10/part10-07_money/src/money.py
@@ -7,11 +7,18 @@ class Money:
     def __str__(self):
         cents = '0' + str(self.cents) if self.cents < 10 else self.cents
         return f'{self.euros}.{cents} eur'
+    
+    def __eq__(self, another: 'Money') -> bool:
+        return (self.euros == another.euros) and (self.cents == another.cents)
 
 
 if __name__ == '__main__':
     e1 = Money(4, 10)
-    e2 = Money(2, 5)  # two euros and five cents
+    e2 = Money(2, 5)
+    e3 = Money(4, 10)
 
     print(e1)
     print(e2)
+    print(e3)
+    print(e1 == e2)
+    print(e1 == e3)
