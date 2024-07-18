3e861d2653bf250ff4f33ede9ae4cd57a0b6b44e
Zouhair Sahtout <zouhairsahtout66@gmail.com>
2024-07-18 12:24:55 +0100

    Simple date 2/3

--- Part 10/part10-08_simple_date/src/simple_date.py
+++ Part 10/part10-08_simple_date/src/simple_date.py
@@ -45,17 +45,40 @@ class SimpleDate:
 
     def __ne__(self, another: 'SimpleDate') -> bool:
         return self.dd != another.dd or self.mm != another.mm or self.yy != another.yy
+    
+    def __add__(self, n_days: int) -> 'SimpleDate':
+        new_d = SimpleDate(self.dd, self.mm, self.yy)
+
+        for _ in range(n_days):
+            new_d.dd += 1
+
+            if new_d.dd > 30:
+                new_d.dd = 1
+                new_d.mm += 1
+
+                if new_d.mm > 12:
+                    new_d.mm = 1
+                    new_d.yy += 1
+
+        return new_d
 
 
 if __name__ == '__main__':
     d1 = SimpleDate(4, 10, 2020)
     d2 = SimpleDate(28, 12, 1985)
-    d3 = SimpleDate(28, 12, 1985)
+    d3 = d1 + 3
+    d4 = d2 + 400
+    # d3 = SimpleDate(28, 12, 1985)
+
+    # print(d1)
+    # print(d2)
+    # print(d1 == d2)
+    # print(d1 != d2)
+    # # print(d1 == d3)
+    # print(d1 < d2)
+    # print(d1 > d2)
 
     print(d1)
     print(d2)
-    print(d1 == d2)
-    print(d1 != d2)
-    print(d1 == d3)
-    print(d1 < d2)
-    print(d1 > d2)
+    print(d3)
+    print(d4)
