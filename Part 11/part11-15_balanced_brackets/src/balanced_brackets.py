d0ec76b91978b6001a3a98f74ded8c8b9fabf1ca
Zouhair Sahtout <zouhairsahtout66@gmail.com>
2024-08-01 20:18:41 +0100

    Balance all the brackets - using functional programming

--- Part 11/part11-15_balanced_brackets/src/balanced_brackets.py
+++ Part 11/part11-15_balanced_brackets/src/balanced_brackets.py
@@ -1,24 +1,41 @@
 def balanced_brackets(my_string: str):
+    # First attempt
+    # if len(my_string) == 0:
+    #     return True
+    
+    # # Get ride of the first character if it's not any bracket
+    # if not my_string[0] in '()[]':
+    #     return balanced_brackets(my_string[1:])
+
+    # # Get ride of the last character if it's not any bracket
+    # if not my_string[-1] in '()[]':
+    #     return balanced_brackets(my_string[:-1])
+    
+    # # Now we know that the first and the last characters are brackets
+    # # Check if they are pair
+    # if my_string[0] == '(' and my_string[-1] == ')':
+    #     return balanced_brackets(my_string[1:-1])
+
+    # if my_string[0] == '[' and my_string[-1] == ']':
+    #     return balanced_brackets(my_string[1:-1])
+    
+    # # They are not!
+    # return False
+
+
+    # Second attempt - using functional programming
+    # Get ride of all the characters that are not brackets
+    my_string: str = ''.join([character for character in my_string if character in '()[]'])
+
     if len(my_string) == 0:
         return True
     
-    # Get ride of the first character if it's not any bracket
-    if not my_string[0] in '()[]':
-        return balanced_brackets(my_string[1:])
-
-    # Get ride of the last character if it's not any bracket
-    if not my_string[-1] in '()[]':
-        return balanced_brackets(my_string[:-1])
-    
-    # Now we know that the first and the last characters are brackets
-    # Check if they are pair
     if my_string[0] == '(' and my_string[-1] == ')':
         return balanced_brackets(my_string[1:-1])
-
+    
     if my_string[0] == '[' and my_string[-1] == ']':
         return balanced_brackets(my_string[1:-1])
     
-    # They are not!
     return False
 
 
