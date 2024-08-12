d11e4e4023dcdc9d923b18ad432392ddc2a754a2
Zouhair Sahtout <zouhairsahtout66@gmail.com>
2024-08-12 15:43:21 +0100

    OrderBook 4/4

--- Part 11/part11-18_order_book/src/order_book.py
+++ Part 11/part11-18_order_book/src/order_book.py
@@ -1,15 +1,18 @@
 # Write your solution here:
 class Task:
-    id = 1
+    id = 0
+
+    @classmethod
+    def new_id(cls) -> int:
+        Task.id += 1
+        return Task.id
 
     def __init__(self, description: str, programmer: str, workload: int) -> None:
         self.description = description
         self.programmer = programmer
         self.workload = workload
         self.finished = False
-
-        self.id = Task.id
-        Task.id += 1
+        self.id = Task.new_id()
 
     def is_finished(self) -> bool:
         return self.finished
@@ -24,30 +27,41 @@ class Task:
 
 class OrderBook:
     def __init__(self) -> None:
-        self.tasks = []
+        self.__tasks = []
 
     def add_order(self, description: str, programmer: str, workload: int) -> None:
-        self.tasks.append(Task(description, programmer, workload))
+        self.__tasks.append(Task(description, programmer, workload))
 
     def all_orders(self) -> list:
-        return self.tasks
+        return self.__tasks
     
     def programmers(self) -> list:
-        return list(set(task.programmer for task in self.tasks))
-    
+        return list(set(task.programmer for task in self.__tasks))
+
     def finished_orders(self) -> list:
-        return [task for task in self.tasks if task.is_finished()]
+        return [task for task in self.__tasks if task.is_finished()]
     
     def unfinished_orders(self) -> None:
-        return [task for task in self.tasks if not task.is_finished()]
+        return [task for task in self.__tasks if not task.is_finished()]
 
     def mark_finished(self, id: int) -> None:
-        ids = [task.id for task in self.tasks]
+        for task in self.__tasks:
+            if task.id == id:
+                task.mark_finished()
+                return
+        raise ValueError('Wrong id!')
 
-        if id in ids:
-            [task.mark_finished() for task in self.unfinished_orders() if task.id == id]
-        else:
-            raise ValueError('The given id is not valid!')
+    def status_of_programmer(self, programmer: str) -> tuple:
+        if programmer not in self.programmers():
+            raise ValueError('Programmer does not exists!')
+        
+        finished_tasks = [task for task in self.finished_orders() if task.programmer == programmer]
+        not_finished_tasks = [task for task in self.unfinished_orders() if task.programmer == programmer]
+
+        finished_hours = sum([task.workload for task in finished_tasks])
+        not_finished_hours = sum([task.workload for task in not_finished_tasks])
+
+        return (len(finished_tasks), len(not_finished_tasks), finished_hours, not_finished_hours)
 
 
 if __name__ == '__main__':
@@ -63,18 +77,30 @@ if __name__ == '__main__':
     # print(t2)
     # print(t3)
 
+    # orders = OrderBook()
+    # orders.add_order("program webstore", "Adele", 10)
+    # orders.add_order("program mobile app for workload accounting", "Eric", 25)
+    # orders.add_order("program app for practising mathematics", "Adele", 100)
+
+    # orders.mark_finished(1)
+    # orders.mark_finished(2)
+
+    # for order in orders.all_orders():
+    #     print(order)
+
+    # print()
+
+    # for programmer in orders.programmers():
+    #     print(programmer)
+
     orders = OrderBook()
     orders.add_order("program webstore", "Adele", 10)
-    orders.add_order("program mobile app for workload accounting", "Eric", 25)
+    orders.add_order("program mobile app for workload accounting", "Adele", 25)
     orders.add_order("program app for practising mathematics", "Adele", 100)
+    orders.add_order("program the next facebook", "Eric", 1000)
 
     orders.mark_finished(1)
     orders.mark_finished(2)
 
-    for order in orders.all_orders():
-        print(order)
-
-    print()
-
-    for programmer in orders.programmers():
-        print(programmer)
+    status = orders.status_of_programmer("Adele")
+    print(status)
