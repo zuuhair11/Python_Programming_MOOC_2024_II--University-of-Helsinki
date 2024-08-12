676924f8c1d394bb95bd8e728e2123c431a85eac
Zouhair Sahtout <zouhairsahtout66@gmail.com>
2024-08-12 10:51:14 +0100

    OrderBook 1/4

---
+++ Part 11/part11-18_order_book/src/order_book.py
@@ -0,0 +1,36 @@
+# Write your solution here:
+class Task:
+    id = 1
+
+    def __init__(self, description: str, programmer: str, workload: int) -> None:
+        self.description = description
+        self.programmer = programmer
+        self.workload = workload
+        self.finished = False
+
+        self.id = Task.id
+        Task.id += 1
+
+    def is_finished(self) -> bool:
+        return self.finished
+
+    def mark_finished(self) -> None:
+        self.finished = True
+
+    def __str__(self) -> str:
+        status = 'FINISHED' if self.finished else 'NOT FINISHED'
+        return f'{self.id}: {self.description} ({self.workload} hours), programmer {self.programmer} {status}'
+
+
+if __name__ == '__main__':
+    t1 = Task('program hello world', 'Eric', 3)
+    print(t1.id, t1.description, t1.programmer, t1.workload)
+    print(t1)
+    print(t1.is_finished())
+    t1.mark_finished()
+    print(t1)
+    print(t1.is_finished())
+    t2 = Task('program webstore', 'Adele', 10)
+    t3 = Task('program mobile app for workload accounting', 'Eric', 25)
+    print(t2)
+    print(t3)
