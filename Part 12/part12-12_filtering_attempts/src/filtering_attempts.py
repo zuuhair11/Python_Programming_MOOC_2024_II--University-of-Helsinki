8ee1da6fd77ba06ad9718bca589b4fb74f2b5f9d
Zouhair Sahtout <zouhairsahtout66@gmail.com>
2024-08-21 14:43:24 +0100

    Filtering attempts 2/3

--- Part 12/part12-12_filtering_attempts/src/filtering_attempts.py
+++ Part 12/part12-12_filtering_attempts/src/filtering_attempts.py
@@ -9,7 +9,7 @@ class CourseAttempt:
 
 
 def accepted(attempts: list) -> list:
-    """First attempt - using normal function"""
+    """First attempt - using named function"""
     def at_least_one(course: CourseAttempt) -> bool:
         return course.grade > 0
 
@@ -22,10 +22,31 @@ def accepted(attempts: list) -> list:
     return [course for course in attempts if course.grade > 0]
 
 
+def attempts_with_grade(attempts: list, grade: int) -> list:
+    """First attempt - using lambda"""
+    # return filter(lambda course: course.grade == grade, attempts)
+
+    """Second attempt - using named function"""
+    def check_grade(course: CourseAttempt) -> bool:
+        return course.grade == grade
+
+    return filter(check_grade, attempts)
+
+    """Third attempt - using list comprehension"""
+    return [course for course in attempts if course.grade == grade]
+
+    """Fourth attempt - using list comprehension with named function"""
+    def check_grade(course: CourseAttempt) -> bool:
+        return course.grade == grade
+
+    return [course for course in attempts if check_grade(course)]
+
+
 if __name__ == '__main__':
     s1 = CourseAttempt('Peter Python', 'Introduction to Programming', 3)
     s2 = CourseAttempt('Olivia C. Objective', 'Introduction to Programming', 5)
-    s3 = CourseAttempt('Peter Python', 'Advanced Course in Programming', 0)
+    s3 = CourseAttempt('Peter Python', 'Introduction to AI', 3)
+    s4 = CourseAttempt('Olivia C. Objective', 'Data Structures and Algorithms', 3)
 
-    for attempt in accepted([s1, s2, s3]):
+    for attempt in attempts_with_grade([s1, s2, s3, s4], 3):
         print(attempt)
